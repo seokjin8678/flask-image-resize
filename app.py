9bf73bd5a1badc60fffbec827ef4ba1dc12f7c63
seokjin8678 <seokjin8678@gmail.com>
2024-05-10 01:25:36 +0900

    feat: 9장, 12장 그리드 이미지 합치는 기능 추가

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import datetime
 import uuid
 from io import BytesIO
-from PIL import Image
-from flask import Flask, request, redirect, render_template, Response, session, send_file
 import requests
+from PIL import Image
+from flask import Flask, request, redirect, render_template, session, send_file
 
 app = Flask(__name__)
 app.secret_key = uuid.uuid4().__str__()
@@ -46,6 +47,7 @@ def resize_fixed():
     width = int(request.form['width'])
     height = int(request.form['height'])
     img = Image.open(file.stream)
+
     resize_img = img.resize((width, height))
 
     output_stream = BytesIO()
@@ -60,8 +62,8 @@ def resize_ratio():
     file = request.files['image']
     ratio = int(request.form['ratio'])
     img = Image.open(file.stream)
-    original_width, original_height = img.size
 
+    original_width, original_height = img.size
     new_width = int(original_width * (ratio / 100))
     new_height = int(original_height * (ratio / 100))
     resize_img = img.resize((new_width, new_height))
@@ -80,10 +82,52 @@ def convert():
     img = img.convert('RGB')
 
     output_stream = BytesIO()
-    img.save(output_stream, 'webp', optimize=True, quality=85)
+    img.save(output_stream, format='webp', optimize=True, quality=85)
     output_stream.seek(0)
 
     return send_file(output_stream, mimetype='image/webp', as_attachment=True, download_name=f'{file.filename}.webp')
+
+
+@app.post('/combine-9')
+def combine_9():
+    images = [Image.open(request.files.get(f'image{i}')) for i in range(9)]
+    new_image = combine_images(images)
+
+    output_stream = BytesIO()
+    new_image.save(output_stream, format="webp")
+    output_stream.seek(0)
+
+    return send_file(output_stream, mimetype='image/webp', as_attachment=True,
+                     download_name=f'combine12_{datetime.datetime.now()}.webp')
+
+
+def combine_images(images):
+    width, height = images[0].size
+    total_width = width * 3
+    print(len(images) // 3)
+    total_height = height * (len(images) // 3)
+    new_image = Image.new('RGB', (total_width, total_height), (0, 0, 0))
+    i, j = 0, 0
+    for image in images:
+        new_image.paste(image, box=(i * width, j * height))
+        i += 1
+        if i >= 3:
+            i = 0
+            j += 1
+    return new_image
+
+
+@app.post('/combine-12')
+def combine_12():
+    images = [Image.open(request.files.get(f'image{i}')) for i in range(12)]
+    new_image = combine_images(images)
+
+    output_stream = BytesIO()
+    new_image.save(output_stream, format="webp")
+    output_stream.seek(0)
+
+    return send_file(output_stream, mimetype='image/webp', as_attachment=True,
+                     download_name=f'combine12_{datetime.datetime.now()}.webp')
 
 
 if __name__ == '__main__':
